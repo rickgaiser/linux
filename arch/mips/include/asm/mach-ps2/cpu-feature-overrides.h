e23306979c9b72573c6ac33839a440095bb40a3a
jur <juergenurban@gmx.de>
2010-09-28 21:05:20 +0000

    Fixes for cache and exception handling.

--- arch/mips/include/asm/mach-ps2/cpu-feature-overrides.h
+++ arch/mips/include/asm/mach-ps2/cpu-feature-overrides.h
@@ -3,6 +3,7 @@
 #ifndef __ASM_MACH_PS2_CPU_FEATURE_OVERRIDES_H
 #define __ASM_MACH_PS2_CPU_FEATURE_OVERRIDES_H
 
-#define cpu_has_llsc	0
+#define cpu_has_llsc		0
+#define cpu_has_4k_cache	1 /* TBD: Verify cache functions. */
 
 #endif
