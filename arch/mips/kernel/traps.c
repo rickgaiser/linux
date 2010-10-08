ebe8135dd64016ded8b6986dcddeebcc985a1073
jur <juergenurban@gmx.de>
2010-10-08 23:44:47 +0000

    Added simple default console using SBIOS putc. Checked CPU features. Added bus error workaround in exception handlers (added 2 x nop). Detect cache size, because TX79 CPU has a different cache size.

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -1086,8 +1086,10 @@ asmlinkage void do_mt(struct pt_regs *regs)
 
 asmlinkage void do_dsp(struct pt_regs *regs)
 {
+#ifndef CONFIG_CPU_R5900
 	if (cpu_has_dsp)
 		panic("Unexpected DSP exception\n");
+#endif
 
 	force_sig(SIGILL, current);
 }
