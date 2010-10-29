8a192dcb40dcb1a81e825f51e1f27e5eb5fbb718
kloader <kloader>
2010-10-29 20:30:55 +0000

    Added basic driver infrastructur (copied from 2.4.17). Added working graphic driver (logo is not working).

---
+++ arch/mips/include/asm/mach-ps2/gsfunc.h
@@ -0,0 +1,36 @@
+/*
+ * linux/include/asm-mips/ps2/gsfunc.h
+ *
+ *	Copyright (C) 2000-2002  Sony Computer Entertainment Inc.
+ *	Copyright (C) 2010       Mega Man
+ *
+ * This file is subject to the terms and conditions of the GNU General
+ * Public License Version 2. See the file "COPYING" in the main
+ * directory of this archive for more details.
+ *
+ * $Id$
+ */
+/* TBD: Unfinished state. Rework code. */
+
+#ifndef __ASM_PS2_GSFUNC_H
+#define __ASM_PS2_GSFUNC_H
+
+#include <linux/ps2/dev.h>
+#include <asm/types.h>
+
+int ps2gs_set_gssreg(int reg, u64 val);
+int ps2gs_get_gssreg(int reg, u64 *val);
+int ps2gs_set_gsreg(int reg, u64 val);
+
+int ps2gs_crtmode(struct ps2_crtmode *crtmode, struct ps2_crtmode *old);
+int ps2gs_display(int ch, struct ps2_display *display, struct ps2_display *old);
+int ps2gs_dispfb(int ch, struct ps2_dispfb *dispfb, struct ps2_dispfb *old);
+int ps2gs_pmode(struct ps2_pmode *pmode, struct ps2_pmode *old);
+int ps2gs_screeninfo(struct ps2_screeninfo *info, struct ps2_screeninfo *old);
+int ps2gs_setdpms(int mode);
+int ps2gs_blank(int onoff);
+int ps2gs_reset(int mode);
+
+extern void (*ps2gs_screeninfo_hook)(struct ps2_screeninfo *info);
+
+#endif /* __ASM_PS2_GSFUNC_H */
