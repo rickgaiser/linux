cbf3b2ae75c6f37ada82110a7307eec43dde4f45
jur <juergenurban@gmx.de>
2011-04-25 16:13:18 +0000

    Fixed bad looking debug output in ps2link.

--- arch/mips/ps2/sbios-console.c
+++ arch/mips/ps2/sbios-console.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/console.h>
 #include <linux/init.h>
+#include <linux/string.h>
 
 #include <asm/mach-ps2/ps2.h>
 #include <asm/mach-ps2/sbios.h>
@@ -17,7 +18,7 @@ void prom_putchar(char c)
 
 int ps2_printf(const char *fmt, ...)
 {
-	char buffer[60];
+	char buffer[80];
 	va_list args;
 	int r;
 	int i;
@@ -25,8 +26,16 @@ int ps2_printf(const char *fmt, ...)
 	va_start(args, fmt);
 	r = vsnprintf(buffer, sizeof(buffer), fmt, args);
 	va_end(args);
+	r = strnlen(buffer, sizeof(buffer));
 	for (i = 0; i < r; i++) {
 		prom_putchar(buffer[i]);
+	}
+	if (r >= (sizeof(buffer) - 1)) {
+		// Add carriage return if buffer was too small.
+		prom_putchar('.');
+		prom_putchar('.');
+		prom_putchar('.');
+		prom_putchar('\n');
 	}
 	return r;
 }
