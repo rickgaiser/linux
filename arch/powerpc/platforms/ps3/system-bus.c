d475f1dc13134b0d911acd5ead564a99aa2ea9be
Rick Gaiser <rgaiser@gmail.com>
2015-07-31 10:33:31 +0200

    ps3encdec

--- arch/powerpc/platforms/ps3/system-bus.c
+++ arch/powerpc/platforms/ps3/system-bus.c
@@ -175,6 +175,7 @@ int ps3_open_hv_device(struct ps3_system_bus_device *dev)
 	case PS3_MATCH_ID_STOR_ROM:
 	case PS3_MATCH_ID_STOR_FLASH:
 	case PS3_MATCH_ID_STOR_NOR_FLASH:
+	case PS3_MATCH_ID_STOR_ENCDEC:
 		return ps3_open_hv_device_sb(dev);
 
 	case PS3_MATCH_ID_SOUND:
@@ -215,6 +216,7 @@ int ps3_close_hv_device(struct ps3_system_bus_device *dev)
 	case PS3_MATCH_ID_STOR_ROM:
 	case PS3_MATCH_ID_STOR_FLASH:
 	case PS3_MATCH_ID_STOR_NOR_FLASH:
+	case PS3_MATCH_ID_STOR_ENCDEC:
 		return ps3_close_hv_device_sb(dev);
 
 	case PS3_MATCH_ID_SOUND:
