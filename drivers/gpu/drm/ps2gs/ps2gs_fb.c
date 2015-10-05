1588efa0ae2f50a379ef7c892d4801c391c36f5d
Rick Gaiser <rgaiser@gmail.com>
2015-10-05 22:13:35 +0200

    ps2 gs: Copy as big as possible blocks, in one scatter gather list

--- drivers/gpu/drm/ps2gs/ps2gs_fb.c
+++ drivers/gpu/drm/ps2gs/ps2gs_fb.c
@@ -12,10 +12,8 @@
 
 #include <linux/ps2/gs.h>
 
-#include <asm/mach-ps2/ps2.h>
 #include <asm/mach-ps2/gsfunc.h>
 #include <asm/mach-ps2/eedev.h>
-#include <asm/mach-ps2/dma.h>
 
 #include <drm/drmP.h>
 #include <drm/drm_crtc.h>
@@ -28,15 +26,9 @@
 #include "ps2gs_drv.h"
 
 
-/** Alignment for width in Bytes. */
-#define PS2_FBMEM_ALIGN 8
-
 #define BUF_SIZE	(4 * 1024)
 static unsigned char buf[BUF_SIZE] __attribute__ ((aligned(16)));
 
-//#define USE_SGL
-
-#ifdef USE_SGL
 #define SGL_MAX 256
 struct ps2fb_priv {
 	/* Scatter gather list */
@@ -44,7 +36,6 @@ struct ps2fb_priv {
 	int sgl_used;
 };
 static struct ps2fb_priv ps2fb;
-#endif // USE_SGL
 
 /*
  * Copied from drivers/gpu/drm/drm_fB_cma_helper.c
@@ -62,32 +53,26 @@ static inline struct drm_fb_cma *to_fb_cma(struct drm_framebuffer *fb)
 	return container_of(fb, struct drm_fb_cma, fb);
 }
 
-#ifdef USE_SGL
 static void ps2fb_sgl_kick(void){
-	FUNC_DEBUG();
+	//FUNC_DEBUG();
 
 	if (ps2fb.sgl_used > 0) {
 		ps2gs_dma_send_sg(ps2fb.sgl, ps2fb.sgl_used);
 		ps2fb.sgl_used = 0;
 	}
 }
-#endif // USE_SGL
 
 static void ps2fb_sgl_add_cont(const void *ptr, unsigned int len)
 {
 	//FUNC_DEBUG();
 	//printk("ps2gs: %s(0x%p, %d)\n", __func__, ptr, len);
 
-#ifdef USE_SGL
 	sg_set_buf(&ps2fb.sgl[ps2fb.sgl_used], ptr, len);
 
 	ps2fb.sgl_used++;
 	if (ps2fb.sgl_used == SGL_MAX) {
 		ps2fb_sgl_kick();
 	}
-#else
-	ps2gs_dma_send(ptr, len);
-#endif // USE_SGL
 }
 
 #if 0
@@ -160,143 +145,84 @@ static void ps2fb_sgl_add(const void *ptr, unsigned int len)
 }
 #endif
 
-/**
- *		Copy data to framebuffer on GS side.
- *
- *		@param info PS2 screeninfo (16 bit and 32 bit PSM is supported)
- *		@param sx Destination X coordinate in framebuffer
- *		@param sy Destination Y coordinate in framebuffer
- *		@param width Width of frame
- *		@param height Height of frame
- *		@param data Source pointer which was allocated with rvmalloc()
- */
-static u64 *ps2fb_copyframe(u64 *gsp, struct ps2_screeninfo *info, int sx, int sy, int width, int height, const void *data)
-{
-	void *gsp_h;
-	int fbw = (info->w + 63) / 64;
-	int bpp;
-
-	//FUNC_DEBUG();
-	//printk("ps2gs: %s(%d, %d, %d, %d, 0x%p)\n", __func__, sx, sy, width, height, data);
-
-	/* Calculate number of bytes per pixel. */
-	switch (info->psm) {
-	case PS2_GS_PSMCT32:
-	case PS2_GS_PSMZ32:
-		bpp = 4;
-		break;
-	case PS2_GS_PSMCT24:
-	case PS2_GS_PSMZ24:
-		bpp = 3;
-		break;
-	case PS2_GS_PSMCT16:
-	case PS2_GS_PSMCT16S:
-	case PS2_GS_PSMZ16:
-	case PS2_GS_PSMZ16S:
-		bpp = 2;
-		break;
-	case PS2_GS_PSMT8:
-	case PS2_GS_PSMT8H:
-	default:
-		bpp = 1;
-		break;
-	}
-	gsp_h = gsp;
-
-	*gsp++ = PS2_GIFTAG_SET_TOPHALF(4, 0, 0, 0, PS2_GIFTAG_FLG_PACKED, 1);
-	/* A+D */
-	*gsp++ = 0x0e;
-
-	*gsp++ = (u64)0 | ((u64)info->fbp << 32) |
-	    ((u64)fbw << 48) | ((u64)info->psm << 56);
-	*gsp++ = PS2_GS_BITBLTBUF;
-
-	*gsp++ = PACK64(0, PACK32(sx, sy));
-	*gsp++ = PS2_GS_TRXPOS;
-
-	*gsp++ = PACK64(width, height);
-	*gsp++ = PS2_GS_TRXREG;
-
-	/* host to local */
-	*gsp++ = 0;
-	*gsp++ = PS2_GS_TRXDIR;
-
-	*gsp++ = PS2_GIFTAG_SET_TOPHALF(ALIGN16(bpp * width * height) / 16, 1, 0, 0, PS2_GIFTAG_FLG_IMAGE, 0);
-	*gsp++ = 0;
-
-	ps2fb_sgl_add_cont(gsp_h, ((unsigned long) gsp) - ((unsigned long) gsp_h));
-#if 0
-	ps2fb_sgl_add(data, ALIGN16(bpp * width * height));
-#else
-	ps2fb_sgl_add_cont(data, ALIGN16(bpp * width * height));
-#endif
-
-	return gsp;
-}
-
+/* QWC   field is 16 bits, so 2^16-1 quad words MAX */
+#define PS2_MAX_DMA_SIZE ((64*1024-1)*16)
+/* NLOOP field is 15 bits, so 2^15-1 quad words MAX */
+#define PS2_MAX_GIF_SIZE ((32*1024-1)*16)
 void ps2gs_fb_redraw(struct drm_framebuffer *fb)
 {
 	struct drm_fb_cma *fb_cma = to_fb_cma(fb);
 	struct drm_gem_cma_object *obj = fb_cma->obj[0];
-	struct ps2_screeninfo ps2_info;
 	u64 *gsp = (u64 *)buf;
-	unsigned long offset;
+	void *gsp_h;
+	unsigned long stride;
 	unsigned long y;
 	unsigned long maxheight;
+	unsigned long fbw, fbp, psm, bpp;
+	void * fb_vaddr;
+
+	//FUNC_DEBUG();
+	//printk("ps2gs: %s: %dx%dx%dbpp (vaddr=0x%p, paddr=0x%p)\n", __func__, fb->width, fb->height, fb->bits_per_pixel, obj->vaddr, (void *)obj->paddr);
 
 	/*
 	 * Consistent DMA mappings are allocated in uncached memory area. But the
 	 * DMA engine expects scatter gather lists from cached memory. So
 	 * convert to cached memory.
 	 */
-	if ((unsigned long)obj->vaddr >= UNCAC_BASE)
-		obj->vaddr = CAC_ADDR(obj->vaddr);
+	fb_vaddr = obj->vaddr;
+	if ((unsigned long)fb_vaddr >= UNCAC_BASE)
+		fb_vaddr = CAC_ADDR(fb_vaddr);
 
-	//FUNC_DEBUG();
-	//printk("ps2gs: %s: %dx%dx%dbpp (vaddr=0x%p, paddr=0x%p)\n", __func__, fb->width, fb->height, fb->bits_per_pixel, obj->vaddr, (void *)obj->paddr);
+	bpp = fb->bits_per_pixel / 8;
+	fbw = (fb->width + 63) / 64;
+	fbp = 0; 			/* FIXME: this is the address in GS */
+	psm = PS2_GS_PSMCT24;		/* FIXME */
 
-	switch (fb->width) {
-		case 640:
-			maxheight = 64;
-			break;
-		case 720:
-			maxheight = 56;
-			break;
-		case 800:
-			maxheight = 50;
-			break;
-		case 1024:
-			maxheight = 40;
-			break;
-		case 1280:
-			maxheight = 32;
-			break;
-		case 1920:
-			maxheight = 20;
-			break;
-		default:
-			maxheight = 20;
-			break;
-	}
+	stride = bpp * fb->width;
+	maxheight = PS2_MAX_GIF_SIZE / stride;
 
-	/* HACK the needed info in here */
-	ps2_info.w   = fb->width;
-	ps2_info.psm = PS2_GS_PSMCT24;
-	ps2_info.fbp = 0;
+	gsp_h = gsp;
 
-	offset = ((fb->bits_per_pixel/8) * fb->width + PS2_FBMEM_ALIGN - 1) & ~(PS2_FBMEM_ALIGN - 1);
+	*gsp++ = PS2_GIFTAG_SET_TOPHALF(4, 0, 0, 0, PS2_GIFTAG_FLG_PACKED, 1);
+	/* A+D */
+	*gsp++ = 0x0e;
+
+	*gsp++ = (u64)0 | ((u64)fbp << 32) | ((u64)fbw << 48) | ((u64)psm << 56);
+	*gsp++ = PS2_GS_BITBLTBUF;
+
+	*gsp++ = PACK64(0, PACK32(0, 0));
+	*gsp++ = PS2_GS_TRXPOS;
+
+	*gsp++ = PACK64(fb->width, fb->height);
+	*gsp++ = PS2_GS_TRXREG;
+
+	/* host to local */
+	*gsp++ = 0;
+	*gsp++ = PS2_GS_TRXDIR;
+
 	for (y = 0; y < fb->height; y += maxheight) {
 		int h;
+		int size;
 
 		h = fb->height - y;
 		if (h > maxheight) {
 			h = maxheight;
 		}
-		gsp = ps2fb_copyframe(gsp, &ps2_info, 0, y, fb->width, h, obj->vaddr + (y * offset));
+
+		size = stride * h;
+
+		*gsp++ = PS2_GIFTAG_SET_TOPHALF(size / 16, 1, 0, 0, PS2_GIFTAG_FLG_IMAGE, 0);
+		*gsp++ = 0;
+
+		ps2fb_sgl_add_cont(gsp_h, ((unsigned long) gsp) - ((unsigned long) gsp_h));
+		gsp_h = gsp;
+#if 0
+		ps2fb_sgl_add(data, ALIGN16(bpp * width * height));
+#else
+		ps2fb_sgl_add_cont(fb_vaddr + (y * stride), size);
+#endif
 	}
-#ifdef USE_SGL
 	ps2fb_sgl_kick();
-#endif // USE_SGL
 }
 
 int
@@ -304,14 +230,12 @@ ps2gs_fb_init(void)
 {
 	FUNC_DEBUG();
 
-#ifdef USE_SGL
 	ps2fb.sgl = vzalloc(SGL_MAX * sizeof(struct scatterlist));
 	if (!ps2fb.sgl) {
 		return -ENOMEM;
 	}
 	sg_init_table(ps2fb.sgl, SGL_MAX);
 	ps2fb.sgl_used = 0;
-#endif // USE_SGL
 
 	return 0;
 }
